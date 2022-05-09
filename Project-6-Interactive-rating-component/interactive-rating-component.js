95f1d3fc926172a7240f24a428dc9683350db13b
Dominik Adamczewski <dominik_adamczewski@wp.pl>
2022-05-09 21:25:19 +0200

    Replace html onclick with JS solution + add media query

--- Project-6-Interactive-rating-component/interactive-rating-component.js
+++ Project-6-Interactive-rating-component/interactive-rating-component.js
@@ -1,9 +1,19 @@
-function onClick(numberId) {
+function onClick() {
+    let element = this.innerHTML;
     let number = document.getElementById('num');
-    number.innerHTML = numberId;
+    number.innerHTML = element;
 }
 
 function replace() {
     document.getElementById('initial_component').style.display="none";
     document.getElementById('thank_you_component').style.display="initial";
 };
+
+document.getElementById('1').onclick = onClick;
+document.getElementById('2').onclick = onClick;
+document.getElementById('3').onclick = onClick;
+document.getElementById('4').onclick = onClick;
+document.getElementById('5').onclick = onClick;
+
+const submitButton = document.getElementById('submit_button');
+submitButton.addEventListener('click', replace);
