510933fa70dd22838268dae1d200e0642547bc5f
Dominik Adamczewski <dominik_adamczewski@wp.pl>
2022-05-24 19:44:08 +0200

    Fix arrow buttons posisiton and accomplish the same effect, with less JS

--- Project-7-faq-accordion-card-component/faq-accordion-card.js
+++ Project-7-faq-accordion-card-component/faq-accordion-card.js
@@ -1,18 +1,14 @@
 function closeOtherAccordions() {
     let activeAnswers = document.querySelectorAll('.answer_active');
     let activeQuestions = document.querySelectorAll('.question.active');
-    let arrowsActive = document.querySelectorAll('.arrow.arrow-active');
         activeAnswers.forEach( function (element) {
             if(element.classList.contains('answer_active')) {
                 element.classList.remove('answer_active');
             }
         activeQuestions.forEach(function (active_q) {
             active_q.classList.remove('active');
-        })
-        arrowsActive.forEach(function (arrAc) {
-            arrAc.classList.remove('arrow-active');
-        })    
-        });
+        })  
+    });
 }
 
 let questions = document.querySelectorAll('.question');
@@ -20,18 +16,15 @@ let questions = document.querySelectorAll('.question');
 questions.forEach(function (question) {
     question.addEventListener('click', function() {
         const answer = this.nextElementSibling;
-        let arrow = this.firstElementChild;
 
 
         if(!this.classList.contains('active')) {
             closeOtherAccordions();
             answer.classList.add('answer_active');
-            arrow.classList.add('arrow-active');
             question.classList.toggle('active');
         } else {
             answer.classList.remove('answer_active');
             this.classList.remove('active');
-            arrow.classList.remove('arrow-active');
         }
     })
 });
