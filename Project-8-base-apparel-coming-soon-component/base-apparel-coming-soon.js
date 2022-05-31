897f9f2942e1ccf50d2abe934477e165dad69da4
Dominik Adamczewski <dominik_adamczewski@wp.pl>
2022-05-31 22:27:32 +0200

    Try to add regexp

--- Project-8-base-apparel-coming-soon-component/base-apparel-coming-soon.js
+++ Project-8-base-apparel-coming-soon-component/base-apparel-coming-soon.js
@@ -2,11 +2,15 @@ const errorMsg = document.querySelector('.error_msg');
 const errorIcon = document.querySelector('.error_icon');
 const form = document.querySelector('form');
 const email = document.querySelector('#email');
+let emailValue = email.value;
 
 function validateForm() {
+    let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
     if(email.value === '') {
         errorMsg.style.visibility = 'visible';
         errorIcon.style.visibility = 'visible';
+    } else if(regex.test(emailValue)) {
+        console.log('Dupsko!')
     } else {
         document.location.reload();
     }
