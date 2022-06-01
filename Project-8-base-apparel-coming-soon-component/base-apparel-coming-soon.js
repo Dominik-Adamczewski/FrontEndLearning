707a67e68b371d691f73f7061e89b5f2d727f2c5
Dominik Adamczewski <dominik_adamczewski@wp.pl>
2022-06-01 19:35:18 +0200

    Add regex validation for the input

--- Project-8-base-apparel-coming-soon-component/base-apparel-coming-soon.js
+++ Project-8-base-apparel-coming-soon-component/base-apparel-coming-soon.js
@@ -2,16 +2,21 @@ const errorMsg = document.querySelector('.error_msg');
 const errorIcon = document.querySelector('.error_icon');
 const form = document.querySelector('form');
 const email = document.querySelector('#email');
-let emailValue = email.value;
+
+function showError() {
+    errorMsg.style.visibility = 'visible';
+    errorIcon.style.visibility = 'visible';
+}
 
 function validateForm() {
     let regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
     if(email.value === '') {
-        errorMsg.style.visibility = 'visible';
-        errorIcon.style.visibility = 'visible';
-    } else if(regex.test(emailValue)) {
-        console.log('Dupsko!')
+        showError();
+    } else if(!regex.test(email.value)) {
+        errorMsg.textContent = 'This is not a valid email';
+        showError();
     } else {
+        alert('Thank you for leaving your email address! We will get back to you shortly!');
         document.location.reload();
     }
 }
