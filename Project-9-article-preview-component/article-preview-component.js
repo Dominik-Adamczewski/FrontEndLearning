e8434f2e2047f1141b5c705f6859380a00624340
Dominik Adamczewski <dominik_adamczewski@wp.pl>
2022-06-09 22:01:07 +0200

    Project 9: Fixes after CR

--- Project-9-article-preview-component/article-preview-component.js
+++ Project-9-article-preview-component/article-preview-component.js
@@ -2,9 +2,5 @@ const shareIcon = document.querySelector('.share-icon');
 const socialIcons = document.querySelector('.social-icons-container');
 
 shareIcon.addEventListener('click', () => {
-    if(socialIcons.classList.contains('not-active')) {
-        socialIcons.classList.remove('not-active');
-    } else {
-        socialIcons.classList.add('not-active');
-    }
+    socialIcons.classList.toggle('not-active');
 })
