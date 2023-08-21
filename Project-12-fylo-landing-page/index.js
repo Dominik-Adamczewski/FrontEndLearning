0f9b97c6584a1fc04d8fa143da3706dcf31ed76b
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2023-08-21 20:40:30 +0200

    Fixes after CR

--- Project-12-fylo-landing-page/index.js
+++ Project-12-fylo-landing-page/index.js
@@ -1,6 +1,6 @@
-const submitButton = document.querySelector('button[type="submit"]');
-const errorMessage = document.querySelector('span.error');
-const input = document.querySelector('form input');
+const submitButton = document.querySelector('.submit-button');
+const errorMessage = document.querySelector('span.error-message');
+const input = document.querySelector('.email-input');
 
 function validateEmailAddress(emailAddress) {
     const regex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
@@ -12,7 +12,7 @@ function displayErrorMessage() {
     errorMessage.classList.add('active');
 }
 
-function removeErrorMessage() {
+function hideErrorMessage() {
     errorMessage.classList.remove('active');
 }
 
@@ -31,5 +31,5 @@ submitButton.addEventListener('click', e => {
 });
 
 input.addEventListener('keydown', () => {
-    removeErrorMessage();
+    hideErrorMessage();
 });
