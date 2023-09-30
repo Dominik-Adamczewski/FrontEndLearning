3166e0c50fed7b577c2b5a62da1f207b7a52dc5c
Dominik-Adamczewski <dominik_adamczewski@wp.pl>
2023-09-30 11:18:34 +0200

    Fix mobile view and add more validations

--- Project-5-Component-with-sign-up-form/component-with-sign-up-form.js
+++ Project-5-Component-with-sign-up-form/component-with-sign-up-form.js
@@ -3,6 +3,7 @@ const firstNameInput = document.getElementById('fname');
 const lastNameInput = document.getElementById('lname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const inputs = document.querySelectorAll('input');
 
 let errorMessages = false;
 
@@ -11,6 +12,50 @@ form.addEventListener('submit', (e) => {
 
     checkInputs();
 });
+
+inputs.forEach(input => {
+    input.addEventListener('keypress', () => {
+        clearErrors(input);
+    });
+});
+
+function clearErrors(input) {
+    const errorClass = 'error';
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    const img = formControl.querySelector('img');
+
+    formControl.classList.remove(errorClass);
+    img.classList.remove(errorClass);
+    small.classList.remove(errorClass);
+    input.classList.remove(errorClass);
+    errorMessages = false;
+}
+
+function setErrorFor(input, message) {
+    const errorClass = 'error';
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    const img = formControl.querySelector('img');
+
+    small.innerText = message;
+
+    formControl.classList.add(errorClass);
+    img.classList.add(errorClass);
+    small.classList.add(errorClass);
+    input.classList.add(errorClass);
+    errorMessages = true;
+}
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+function validateLength(value) {
+    const regex = /^.{8,}$/;
+    return regex.test(value);
+}
 
 function checkInputs() {
     // get the values from inputs
@@ -35,28 +80,21 @@ function checkInputs() {
         // show error
         // add error class
         setErrorFor(email, 'Looks like this is not en email');
+    } else if(!validateEmail(emailValue)) {
+        setErrorFor(email, 'Please enter a valid email address')
     }
 
     if(passwordValue === '') {
         //show error
         // add error class
         setErrorFor(password, 'Password cannot be empty');
+    } else if(!validateLength(passwordValue)) {
+        console.log('dupa');
+        setErrorFor(password, 'Your password needs to be at least 8 characters long')
     }
 
     if(errorMessages === false) {
         alert('Form submitted successfully!');
+        location.reload();
     }
-}
-
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    const img = formControl.querySelector('img');
-
-    small.innerText = message;
-
-    formControl.className = 'form_control error';
-    img.className = 'exclamation-circle error';
-    small.className = 'error_msg error';
-    errorMessages = true;
 }
